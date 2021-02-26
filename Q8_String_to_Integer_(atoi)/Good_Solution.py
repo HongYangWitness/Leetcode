3e842e17a713d0ed8e785d4496b86f6aea5924b3
HongYangWitness <yanghongwitness@gmail.com>
2021-02-26 10:15:39 +0800

    Feb 26, 2021

--- Q8_String_to_Integer_(atoi)/Good_Solution.py
+++ Q8_String_to_Integer_(atoi)/Good_Solution.py
@@ -1,15 +1,13 @@
 """
 Good Solution:
 """
+import re
 class Solution:
-    def lengthOfLongestSubstring(self, s: str) -> int:
-        k, res, c_dict = -1, 0, {}#字典key为字符串值，value为出现的位置
-        for i, c in enumerate(s):#i为位置，c为字符串值
-            if c in c_dict and c_dict[c] > k:  # 字符c在字典中 且 上次出现的下标大于当前子串的起始下标k
-                k = c_dict[c] #当前子串的起始下标更新  
-				#c_dict[c] = i
-            else:
-				#c_dict[c] = i
-                res = max(res, i-k)
-			c_dict[c] = i#更新字符串值c的位置，这句话相当于被注释掉的两句话
-        return res
+    def myAtoi(self, str: str) -> int:
+        INT_MAX = 2147483647    
+        INT_MIN = -2147483648
+        str = str.lstrip()      #清除左边多余的空格
+        num_re = re.compile(r'^[\+\-]?\d+')   #设置正则规则
+        num = num_re.findall(str)   #查找匹配的内容
+        num = int(*num) #由于返回的是个列表，解包并且转换成整数
+        return max(min(num,INT_MAX),INT_MIN)    #返回值
